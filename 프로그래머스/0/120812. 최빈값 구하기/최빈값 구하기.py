780c8d713cbd66a1af3a7f05fffebc7fa5f6c6b4
k22900 <132814310+k22900@users.noreply.github.com>
2024-12-02 21:48:52 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+def solution(array):
+    answer = 0
+    arrCnt=0
+    arrCnt=Counter(array)
+    arrCntVal=list(Counter.values(arrCnt))
+    arrCntKey=list(Counter.keys(arrCnt))
+    # print(arrCntVal,arrCntKey)
+    if len(arrCntVal) > 1:
+        numCnt=Counter(arrCntVal)
+        # maxNum=max(arrCntVal)
+        sortedVal=sorted(arrCntVal,reverse=True)
+        # print(sortedVal)
+        # print(arrCntVal,arrCntKey)
+        # print(maxNum)
+        firstBigNum=sortedVal[0]
+        secondBigNum=sortedVal[1]
+        # print(firstBigNum,secondBigNum)
+        if firstBigNum == secondBigNum:
+            res = -1
+        else:
+            res = arrCntKey[arrCntVal.index(firstBigNum)]
+    elif len(arrCntVal) == 1:
+        res= arrCntKey[0]
+    return res
