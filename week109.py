8a2e0ab3c4248d1ab005d4cc1be7c65535c4d036
k22900 <132814310+k22900@users.noreply.github.com>
2025-09-04 20:17:31 +0900

    25.09.04

--- week109.py
+++ week109.py
@@ -36,27 +36,86 @@ class Machine(ABC):
 # 전원 다시키면, 바때리 100
 class Speaker(Machine):
     def __init__(self, brand):
-        super().__init__()
-        self.volume_Level=50
+        self.brand=brand
+        self.__volume_Level=50
+        self.mod_status=False #기본값:base_mod
         
     def power_on(self):
-        self.__battery = 100
+        self.is_on=True
+        self.__battery = 430
         print("전원이 켜짐니다")
 
     def power_off(self):
+        self.is_on=False
         self.__battery = 0
         print("전원이 꺼짐니다")    
         
     def volume_up(self):
+        if self.__battery<=0:
+            print("배터리가 부족합니다")
+            return None
+        self.__battery-=10
+        
+        if self.is_on == False:
+            print("현재 전원이 꺼져있습니다")
+            return None
+        self.__volume_Level=min(self.__volume_Level+5,100)
+        print(f"현재 음량: {self.__volume_Level}\n현재 배터리 잔량은 {self.__battery}입니다")
         pass
     
     def volume_down(self):
+        if self.__battery<=0:
+            print("배터리가 부족합니다")
+            return None
+        self.__battery-=10
+        
+        if self.is_on == False:
+            print("현재 전원이 꺼져있습니다")
+            return None
+        self.__volume_Level=max(self.__volume_Level-5,0)
+        print(f"현재 음량: {self.__volume_Level} \n현재 배터리 잔량은 {self.__battery}입니다")
         pass
     
     def base_mod(self):
-        pass
+        if self.__battery<=0:
+            print("배터리가 부족합니다")
+            return None
+        self.__battery-=10
+        
+        if self.is_on == False:
+            print("현재 전원이 꺼져있습니다")
+            return None
+        if self.mod_status==False:
+            print(f"이미 음향효과 타입이 베이스 입니다 \n현재 배터리 잔량은 {self.__battery}입니다")
+            return None
+        self.mod_status=False
+        print(f"음향효과: base \n현재 배터리 잔량은 {self.__battery}입니다")
     
     def Surround_mod(self):
-        pass
+        if self.__battery<=0:
+            print("배터리가 부족합니다")
+            return None
+        self.__battery-=10
+        
+        if self.is_on == False:
+            print("현재 전원이 꺼져있습니다")
+            return None
+        if self.mod_status==True:
+            print(f"이미 음향효과 타입이 서라운드 입니다 \n현재 배터리 잔량은 {self.__battery}입니다")
+            return None
+        self.mod_status=True
+        print(f"음향효과: surround \n현재 배터리 잔량은 {self.__battery}입니다")
+    def battery_charge(self):
+        self.__battery=100
     
-Speaker("SONY")
+sp=Speaker("SONY")
+sp.power_on()
+sp.volume_down()
+for _ in range(15):
+    sp.volume_down()
+for _ in range(25):
+    sp.volume_up()
+sp.Surround_mod()
+sp.Surround_mod()
+sp.base_mod()
+sp.base_mod()
